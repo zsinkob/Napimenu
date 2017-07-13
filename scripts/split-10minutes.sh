3ff2bf58cb8770378b2ed1b81329affdd0741954
Zsinkó Balázs <zsinkob@gmail.com>
2017-07-13 23:51:14 +0200

    10 minutes

---
+++ scripts/split-10minutes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+outfolder="/var/www/html/ebedmenu/"
+
+dailyfile="10minutes_$(date +%Y-%m-%d)"
+
+echo "${outfolder}${dailyfile}.png"
+$(curl "http://10minutes.hu/images/home_1_06.png" > "${outfolder}${dailyfile}.png")
+convert "${outfolder}${dailyfile}.png" -negate "${outfolder}${dailyfile}.jpg"
+
+$(tesseract "${outfolder}${dailyfile}.jpg" "${outfolder}${dailyfile}" -l hun)
+
